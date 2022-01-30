e0361530c5404269cb8372279291699bc565a739
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-01-30 18:07:23 +0900

    Update bit全探索.cpp

--- Algorithm/bit全探索/bit全探索.cpp
+++ Algorithm/bit全探索/bit全探索.cpp
@@ -3,7 +3,7 @@ for(int x = 0; x < 8; x++){                   //bitの値設定
         sum = keta[0];　　　　　　　　　　　　　　 //比較に用いる合計の初期化
         s = char('0' + keta[0]);　　　　　　　　 //最後に出力する部分の初期化と設定
         for(int i = 0; i < 3; i++){           //bitの桁の設定
-            if(x & (1<<i)){                   //桁のbit判定
+            if(x & (1<<i)){                   //桁のbit判定bitにi番目のフラグが立っているか
                 sum += keta[i+1];
                 s += '+';
                 s += char(keta[i+1] + '0');
