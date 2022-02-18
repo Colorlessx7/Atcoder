bcc98d00a012ecf6a765036ca831cd55a4b314ef
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-02-18 13:08:44 +0900

    Update 177C.cpp

--- ABC/C/177C.cpp
+++ ABC/C/177C.cpp
@@ -19,3 +19,28 @@ int main(){
     cout << ans << endl;
     return 0;
 }
+
+//ACL使用版の追記
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(int i = 0; i < N; i++)
+typedef long long ll;
+using mint = modint1000000007;
+int main(){
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i,n){
+        cin >> a[i];
+    }
+    mint ans = 0;
+    mint x = 0;
+    rep(i,n){
+        ans += a[i] * x;
+        x += a[i];
+    }
+    cout << ans.val() << endl;
+    return 0;
+}
