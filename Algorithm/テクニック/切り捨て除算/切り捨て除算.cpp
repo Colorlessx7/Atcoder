d906e6223324d1663eea04c12e19e2c0f3bab615
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-02-20 16:30:34 +0900

    Create 切り捨て除算.cpp

---
+++ Algorithm/テクニック/切り捨て除算/切り捨て除算.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(int i = 0; i < N; i++)
+#define rep2(i,N) for(int i = 1; i <= N; i++)
+#define rep3(i,N) for(int i = n - 1; i >= 0; i--)
+#define rep4(i,N) for(int i = n; i > 0; i--)
+#define all(x) x.begin(),x.end() 
+typedef long long ll;
+using P = pair<int,int>;
+int main() {
+    ll x ;
+    cin >> x;
+    cout << x / 10 - (x % 10 < 0) << "\n";
+}
