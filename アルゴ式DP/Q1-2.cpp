3012b5602631a9b566e99a4b5c029dec7f68683b
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-03-09 16:03:55 +0900

    Create Q1-2.cpp

---
+++ アルゴ式DP/Q1-2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,N) for(int i = 0; i < N; i++)
+#define rep2(i,N) for(int i = 1; i <= N; i++)
+#define rep3(i,N) for(int i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(int i = N; i > 0; i--)
+#define replr(i,l,r) for(int i = l; i < r; i++)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+typedef long long ll;
+using P = pair<int,int>;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    rep(i,n){
+        cin >> a[i];
+    }
+    vector<int> dp(n);
+    dp[0] = 0;
+    replr(i,1,n){
+        dp[i] += dp[i-1] + a[i];
+        if(i != 1){
+            dp[i] = min(dp[i],dp[i-2] + 2*a[i]);
+        }
+    }
+    cout << dp[n - 1] << endl;
+    return 0;
+}
