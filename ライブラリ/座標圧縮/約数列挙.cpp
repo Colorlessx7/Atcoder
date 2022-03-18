4171d9793c5ded6bacd6b38e0937be3690f83cca
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-03-18 22:25:37 +0900

    Create 約数列挙.cpp

---
+++ ライブラリ/座標圧縮/約数列挙.cpp
@@ -0,0 +1,11 @@
+vector<ll> divisor(ll n) {
+    vector<ll> ret;
+    for (ll i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ret.push_back(i);
+            if (i * i != n) ret.push_back(n / i);
+        }
+    }
+    sort(all(ret));
+    return ret;
+}
