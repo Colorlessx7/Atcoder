c3005b21e8044d6b1d92fcd2172f14e505b4882b
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-03-28 16:34:26 +0900

    Add files via upload

---
+++ ABC/A/244A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i > r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+typedef long long ll;
+using P = pair<ll,ll>;
+int main(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    cout << s[n-1] << endl;
+    return 0;
+}
