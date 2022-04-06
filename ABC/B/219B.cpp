5cfb97cebf3b6e7a30299852d10f811a6012dee2
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-04-06 15:55:09 +0900

    Create 219B.cpp

---
+++ ABC/B/219B.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i > r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  vector<string> s(3);
+  rep(i,3){
+    cin >> s[i];
+  }
+  string t;
+  cin >> t;
+  ll n = t.size();
+  string ans;
+  rep(i,n){
+    ll num = t[i] - '0';
+    num--;
+    ans += s[num];
+  }
+  cout << ans << endl;
+  return 0;
+}
