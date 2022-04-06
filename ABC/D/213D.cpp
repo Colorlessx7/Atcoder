92475203f67505aef554eab38dbd5556647c870c
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-04-06 15:50:30 +0900

    Update 213D.cpp

--- ABC/D/213D.cpp
+++ ABC/D/213D.cpp
@@ -2,47 +2,52 @@
 #include<atcoder/all>
 using namespace atcoder;
 using namespace std;
-#define rep(i,N) for(int i = 0; i < N; i++)
-#define rep2(i,N) for(int i = 1; i <= N; i++)
-#define rep3(i,N) for(int i = N - 1; i >= 0; i--)
-#define rep4(i,N) for(int i = N; i > 0; i--)
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i > r; i--)
 #define all(x) x.begin(),x.end() 
-typedef long long ll;
-using P = pair<int,int>;
-void dfs(int i,int back,vector<bool>& visited,vector<vector<int>> &jump,vector<int>& ans){
-    visited[i] = 1;
-    ans.push_back(i+1);
-    for(int j : jump[i]){
-        if(visited[j]){
-            continue;
-        }
-        dfs(j,i,visited,jump,ans);
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+void dfs(ll u,vector<vector<ll>> &g,vector<ll> &ans,vector<bool> &visit){
+  visit[u] = true;
+  ans.push_back(u+1);
+  for(ll v : g[u]){
+    if(visit[v]){
+      continue;
     }
-    ans.push_back(back+1);
-    return;
+    dfs(v,g,ans,visit);
+    ans.push_back(u+1);
+  }
+  return;
 }
 int main(){
-    int n;
-    cin >> n;
-    vector<vector<int>> jump(n);
-    rep(i,n-1){
-        int a, b;
-        cin >> a >> b;
-        a--; b--;
-        jump[a].push_back(b);
-        jump[b].push_back(a);
-    }
-    rep(i,n){
-        sort(jump[i].begin(),jump[i].end());
-    }
-    vector<bool> visit(n);
-    vector<int> ans;
-    dfs(0,-1,visit,jump,ans);
-    ans.pop_back();
-    int f = ans.size();
-    rep(i,f){
-        cout << ans[i] << " ";
-    }
-    cout << endl;
-    return 0;
+  ll n;
+  cin >> n;
+  vector<vector<ll>> g(n);
+  rep(i,n-1){
+    ll a, b;
+    cin >> a >> b;
+    a--; b--;
+    g[a].push_back(b);
+    g[b].push_back(a);
+  }
+  rep(i,n){
+    sort(all(g[i]));
+  }
+  vector<bool> visit(n);
+  vector<ll> ans;
+  dfs(0,g,ans,visit);
+  ll num = ans.size();
+  rep(i,num){
+    cout << ans[i] << " ";
+  }
+  cout << endl;
+  return 0;
 }
+
