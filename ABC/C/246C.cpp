5bf3902d3b66fbdfc998249e1e0cc8e50e78bfb1
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-04-09 14:56:56 +0900

    Create 246C.cpp

---
+++ ABC/C/246C.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i > r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  ll n, k, x;
+  cin >> n >> k >> x;
+  vector<ll> a(n);
+  ll maxm = 0;
+  rep(i,n){
+    cin >> a[i];
+    maxm += a[i];
+  }
+  ll num = 0;
+  rep(i,n){
+    num += a[i] / x;
+    a[i] %= x;
+  }
+  ll ans = maxm;
+  sort(allr(a));
+  if(num >= k){
+    ans -= k * x;
+  }else {
+    ll diff = k - num;
+    diff = min(diff,n);
+    ans -= num * x;
+    rep(i,diff){
+      ans -= a[i];
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
