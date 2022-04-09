13eda5b9d938be7d6d6c5a69a46a56758a7bd240
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-04-09 14:55:16 +0900

    Create 246B.cpp

---
+++ ABC/B/246B.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i > r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  double a, b;
+  cin >> a >> b;
+  double r = sqrt(a*a+b*b);
+  a /= r; b /= r;
+  printf("%.12f %.12f\n",a,b);
+  return 0;
+}
