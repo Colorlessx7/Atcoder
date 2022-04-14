46529771cf32e445140fc4fef509bcc65a104ae5
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-04-14 14:42:34 +0900

    Create 044B.cpp

---
+++ ABC/B/044B.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  string w;
+  cin >> w;
+  ll n = w.size();
+  map<char,ll> mp;
+  rep(i,n){
+    mp[w[i]]++;
+  }
+  for(auto p : mp){
+    if(p.second % 2 != 0){
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+  cout << "Yes" << endl;
+  return 0;
+}
