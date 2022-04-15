fdb71d337fb3e92c3bd9ae6650c9cd4528f58863
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-04-15 15:34:24 +0900

    Update 149C.cpp

--- ABC/C/149C.cpp
+++ ABC/C/149C.cpp
@@ -1,29 +1,41 @@
 #include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
 using namespace std;
-#define rep(i,N) for(int i = 0; i < N; i++)
-typedef long long ll;
-int Primecheckup(int x){
-    vector<bool> prime(100004,1);
-    prime[0] = 0;
-    prime[1] = 0;
-    for(int i = 2; i * i < 100004; ++i){
-        if(prime[i]){
-            for(int j = 2; i * j < 100004; ++j){
-                prime[i * j] = 0;
-            }
-        }
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+void prime(int x,vector<bool> &prime){
+  prime[0] = 0;
+  prime[1] = 0;
+  for(int i = 2; i * i <= x + 1; ++i){
+    if(prime[i]){
+      for(int j = 2; i * j <= x + 1; ++j){
+        prime[i * j] = 0;
+      }
     }
-    for(int i = x; i < 100004; i++){
-        if(prime[i]){
-            return i;
-        }
-    }
-    return 0;
-}
+  }
+  return;
+}  
 int main(){
-    int x;
-    cin >> x;
-    int ans = Primecheckup(x);
-    cout << ans << endl;
-    return 0;
+  ll x;
+  cin >> x;
+  vector<bool> primes(105000,1);
+  prime(105000,primes);
+  replr(i,x,105000){
+    if(primes[i]){
+      cout << i << endl;
+      return 0;
+    }
+  }
+  return 0;
 }
