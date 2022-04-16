6d612cadc8557767596e62ec50b04416d9a1682c
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-04-16 12:28:30 +0900

    Create 113B.cpp

---
+++ ABC/B/113B.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  double n,t,a;
+  cin >> n >> t >> a;
+  vector<double> h(n);
+  rep(i,n){
+    cin >> h[i];
+  }
+  vector<pair<double,double>> ans(n);
+  rep(i,n){
+    ans[i].first = abs(a - (t - h[i] * 0.006));
+    ans[i].second = i+1;
+  }
+  sort(all(ans));
+  cout << ans[0].second << endl;
+  return 0;
+}
