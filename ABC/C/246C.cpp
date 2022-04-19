77cddb36d223c6b88400ece2c0ddf55b8e5d0452
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-04-19 14:35:43 +0900

    Update 246C.cpp

--- ABC/C/246C.cpp
+++ ABC/C/246C.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
 #define rep4(i,N) for(ll i = N; i > 0; i--)
 #define replr(i,l,r) for(ll i = l; i < r; i++)
-#define reprl(i,l,r) for(ll i = l; i > r; i--)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
 #define all(x) x.begin(),x.end() 
 #define allr(x) x.rbegin(),x.rend() 
 using ll = long long;
@@ -18,28 +18,46 @@ int main(){
   ll n, k, x;
   cin >> n >> k >> x;
   vector<ll> a(n);
-  ll maxm = 0;
+  //クーポン未使用時の必要金額
+  ll sum = 0;
+  //x円カットできるクーポンの回数
+  ll ex = 0;
   rep(i,n){
     cin >> a[i];
-    maxm += a[i];
-  }
-  ll num = 0;
-  rep(i,n){
-    num += a[i] / x;
+    //合計に入力を足す
+    sum += a[i];
+    //入力をxで割った値がx円しっかり値段をカットできるクーポンの枚数になるのでそれを足す
+    ex += a[i] / x;
+    //クーポンでx円ちょうど値段をカットできる回数使い切った後の必要金額にしておく
     a[i] %= x;
   }
-  ll ans = maxm;
+  //最終的な必要金額
+  ll ans = 0;
+  //クーポンをx円ちょうど値段をカットできない対象に使う場合は
+  //値段の高い方からクーポンを使いたいので(貪欲法)値段を降順にしておく
   sort(allr(a));
-  if(num >= k){
-    ans -= k * x;
+  //クーポンでx円ちょうど値下げできる分しかクーポンがない場合の処理
+  if(k <= ex){
+    //クーポンなしの場合の金額からクーポンの枚数n*クーポンの値下げ金額xを
+    //掛けたものを引いたものが最終的な金額となる
+    ans = sum - k * x;
   }else {
-    ll diff = k - num;
-    diff = min(diff,n);
-    ans -= num * x;
-    rep(i,diff){
-      ans -= a[i];
+    //x円ちょうど値下げできるクーポンを使い切ったものとして
+    //考えるためにexを元のクーポンの数kからひく
+    k -= ex;
+    //配列外参照回避のために配列サイズと残りのクーポン数のminをとる
+    //nを超えたクーポンを所持しているならここでnとなりansは最終的に0となる
+    k = min(k,n);
+    //クーポンを使う処理(この時点でa配列の値はx未満なのでクーポンを使うと0円になる)
+    rep(i,k){
+      a[i] = 0;
+    }
+    //クーポンを使って0円にした後のa配列の値の合計が最終的な必要金額となる
+    rep(i,n){
+      ans += a[i];
     }
   }
   cout << ans << endl;
   return 0;
 }
+
