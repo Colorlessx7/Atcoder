1e891aa08ae66e348cd07033f48bd6aae8cf493f
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-04-20 17:06:25 +0900

    Update 211C.cpp

--- ABC/C/211C.cpp
+++ ABC/C/211C.cpp
@@ -2,34 +2,38 @@
 #include<atcoder/all>
 using namespace atcoder;
 using namespace std;
-#define rep(i,N) for(int i = 0; i < N; i++)
-#define rep2(i,N) for(int i = 1; i <= N; i++)
-#define rep3(i,N) for(int i = n - 1; i >= 0; i--)
-#define rep4(i,N) for(int i = n; i > 0; i--)
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
 #define all(x) x.begin(),x.end() 
-typedef long long ll;
-using P = pair<int,int>;
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
 using mint = modint1000000007;
-const int mod = 1000000007;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
 int main(){
-    string s;
-    cin >> s;
-    int n = s.size();
-    vector<vector<int>> dp(n+1, vector<int>(9));
-    rep(i,n+1){
-        dp[i][0] = 1;
+  string s;
+  cin >> s;
+  ll n = s.size();
+  vector<vector<mint>> dp(9,vector<mint>(n+1));
+  rep(i,n+1){
+    dp[0][i] = 1;
+  }
+  string t = "chokudai";
+  rep(i,8){
+    rep(j,n){
+      if(s[j] == t[i]){
+        dp[i+1][j+1] += dp[i][j] + dp[i+1][j];
+      }else {
+        dp[i+1][j+1] += dp[i+1][j];
+      }
     }
-    string t = "chokudai";
-    rep(i,n){
-        rep(j,8){
-            if(s[i] != t[j]){
-                dp[i+1][j+1] = dp[i][j+1];
-            }else {
-                dp[i+1][j+1] = (dp[i][j+1] + dp[i][j]) % mod;
-            }
-        }
-    }
-    cout << dp[n][8] << endl;
-    return 0;
+  }
+  cout << dp[8][n].val() << endl;
+  return 0;
 }
-//DP動的計画法問題 もらうDPver
+
