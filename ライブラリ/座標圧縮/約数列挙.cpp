535409c5fcd9da27b3963bb73d41fe742eded85b
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-04-20 21:33:04 +0900

    Update 約数列挙.cpp

--- ライブラリ/座標圧縮/約数列挙.cpp
+++ ライブラリ/座標圧縮/約数列挙.cpp
@@ -1,12 +1,12 @@
 void divisor(ll n, vector<ll>& div) {
-    for (ll i = 1; i * i <= n; i++) {
-        if (n % i == 0) {
-            div.push_back(i);
-            if (i * i != n) div.push_back(n / i);
-        }
+  for (ll i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      div.push_back(i);
+      if (i * i != n) div.push_back(n / i);
     }
-    sort(all(div));
-    return;
+  }
+  sort(all(div));
+  return;
 }
 //vector<ll> num;
 //divisor(n,num);
