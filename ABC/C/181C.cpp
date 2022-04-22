2ab76b5d50d9d991eaad9df418db0214abda1fe5
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-04-22 20:05:37 +0900

    Update 181C.cpp

--- ABC/C/181C.cpp
+++ ABC/C/181C.cpp
@@ -1,28 +1,40 @@
 #include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
 using namespace std;
-#define rep(i,N) for(int i = 0; i < N; i++)
-typedef long long ll;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
 int main(){
-    int n;
-    cin >> n;
-    vector<double> x(n),y(n);
-    rep(i,n){
-        cin >> x[i] >> y[i];
-    }
-    rep(i,n){
-        rep(j,i){
-            rep(k,j){
-                int a = x[j] - x[i];
-                int b = y[j] - y[i];
-                int c = x[k] - x[i];
-                int d = y[k] - y[i];
-                if(a * d == c * b){
-                    cout << "Yes" << endl;
-                    return 0;
-                }
-            }
+  ll n;
+  cin >> n;
+  vector<ll> x(n),y(n);
+  rep(i,n){
+    cin >> x[i] >> y[i];
+  }
+  rep(i,n){
+    rep(j,i){
+      rep(k,j){
+        ll x1 = x[k] - x[j];
+        ll y1 = y[k] - y[j];
+        ll x2 = x[j] - x[i];
+        ll y2 = y[j] - y[i];
+        if(x1 * y2 == x2 * y1){
+          cout << "Yes" << endl;
+          return 0;
         }
+      }
     }
-    cout << "No" << endl;
-    return 0;
+  }
+  cout << "No" << endl;
+  return 0;
 }
