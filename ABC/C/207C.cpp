a4756d39285aad5ecc8d0907c19f5d219b74dc93
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-04-26 14:41:02 +0900

    Update 207C.cpp

--- ABC/C/207C.cpp
+++ ABC/C/207C.cpp
@@ -2,8 +2,18 @@
 #include<atcoder/all>
 using namespace atcoder;
 using namespace std;
-#define rep(i,N) for(int i = 0; i < N; i++)
-typedef long long ll;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
 int main(){
     int n;
     cin >> n;
