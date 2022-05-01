aad929e24818a8f0ddb2569468c11853dddf045d
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-05-01 20:58:08 +0900

    Create 201B.cpp

---
+++ ABC/B/201B.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+bool compare(pair<string,ll> a, pair<string,ll> b) {
+  if(a.second != b.second){
+    return a.second > b.second;
+  }else{
+    return a.first < b.first;
+  }
+}
+int main(){
+  ll n;
+  cin >> n;
+  vector<pair<string,ll>> st(n);
+  rep(i,n){
+    cin >> st[i].first >> st[i].second;
+  }
+  sort(all(st),compare);
+  cout << st[1].first << endl;
+  return 0;
+}
