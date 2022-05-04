6ee18d3c7e7a43bfa0fba317eea87c19764b1505
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-05-04 14:41:53 +0900

    Create 236B.cpp

---
+++ ABC/B/236B.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  ll n;
+  cin >> n;
+  ll ans = 0;
+  rep(i,4*n-1){
+    ll a;
+    cin >> a;
+    ans ^= a;
+  }
+  cout << ans << endl;
+  return 0;
+}
