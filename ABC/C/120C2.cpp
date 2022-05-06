6d0b7ca657aaedd82b5795070cd0ef4361fb278c
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-05-06 15:19:36 +0900

    Create 120C2.cpp

---
+++ ABC/C/120C2.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  string s;
+  cin >> s;
+  ll n = s.size();
+  ll z = 0, o = 0;
+  rep(i,n){
+    if(s[i] == '0'){
+      z++;
+    }else {
+      o++;
+    }
+  }
+  cout << min(z,o) * 2 << endl;
+  return 0;
+}
