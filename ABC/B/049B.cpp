db59f8b939994484dde86995fbb66f303f08fa3c
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-05-07 17:11:20 +0900

    Create 049B.cpp

---
+++ ABC/B/049B.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  ll h, w;
+  cin >> h >> w;
+  vector<vector<char>> c(h,vector<char>(w));
+  rep(i,h){
+    rep(j,w){
+      cin >> c[i][j];
+    }
+  }
+  rep(i,h){
+    rep(x,2){
+      rep(j,w){
+        cout << c[i][j];
+      }
+      cout << endl;
+    }
+  }
+  return 0;
+}
