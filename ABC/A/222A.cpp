838f2a6ef6705949966a7e85016fbf3e77a2c790
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-05-07 17:10:31 +0900

    Create 222A.cpp

---
+++ ABC/A/222A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  string s;
+  cin >> s;
+  ll n = s.size();
+  ll x = 4 - n;
+  string ans;
+  rep(i,x){
+    ans += '0';
+  }
+  ans += s;
+  cout << ans << endl;
+  return 0;
+}
