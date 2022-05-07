cc23d01d3f8ca76cd5284d559577322e6782da76
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-05-07 16:13:41 +0900

    Create dijkstra_text.cpp

---
+++ Algorithm/ダイクストラ/dijkstra_text.cpp
@@ -0,0 +1,50 @@
+const ll INF = 1LL << 60;
+struct edge{ll to, cost;};
+struct graph{
+  ll V;
+  vector<vector<edge>> G;
+  vector<ll> d;
+  vector<ll> prev;
+  vector<ll> path;
+  graph(ll n){
+    init(n);
+  }
+  void init(ll n){
+    V = n;
+    G.resize(V);
+    d.resize(V,INF);
+    prev.resize(V,-1);
+  }
+  void add_edge(ll s, ll t, ll cost){
+    edge e;
+    e.to = t, e.cost = cost;
+    G[s].push_back(e);
+  }
+  void dijkstra(ll s){
+    rep(i,V){
+      d[i] = INF;
+    }
+    d[s] = 0LL;
+    priority_queue<P,vector<P>, greater<P>> que;
+    que.push(P(0LL,s));
+    while(que.size()){
+      P p = que.top(); que.pop();
+      ll v = p.second;
+      if(d[v] < p.first) continue;
+      for(auto e : G[v]){
+        if(d[e.to] > d[v] + e.cost){
+          d[e.to] = d[v] + e.cost;
+          prev[e.to] = v;
+          que.push(P(d[e.to],e.to));
+        }
+      }
+    }
+  }
+  vector<ll> get(ll t){
+    for(ll i = t; i != -1; i = prev[i]){
+      path.push_back(i);
+    }
+    reverse(all(path));
+    return path;
+  }
+};
