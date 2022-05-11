0620982e21564847b3b3008e81cc951dac389b02
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-05-11 15:06:50 +0900

    Update エラトステネス.cpp

--- ライブラリ/整数系/エラトステネス.cpp
+++ ライブラリ/整数系/エラトステネス.cpp
@@ -1,14 +1,19 @@
-void prime(int x,vector<bool> &prime){
-  prime[0] = 0;
-  prime[1] = 0;
-  for(int i = 2; i * i <= x + 1; ++i){
-    if(prime[i]){
-      for(int j = 2; i * j <= x + 1; ++j){
-        prime[i * j] = 0;
-      }
+void prime(){
+  //0,1は素数ではないので0
+  primes[0] = primes[1] = 0;
+  //最初の素数である2から判定する最大値のMまで回す
+  for(ll i = 2; i <= M; ++i){
+    //primes[i]=0ならiは素数ではないのでコンティニュー
+    if(!primes[i]){
+      continue;
+    }
+    //iは素数なので素数配列dに格納
+    d.push_back(i);
+    //i^2から素数iの倍数[i*j]を全てfalseにする
+    for(ll j = i*i; j <= M; j += i){
+      //素数iの倍数は素数でないのでfalseに
+      primes[i * j] = 0;
     }
   }
   return;
 }
-//vector<bool> primes(x,1);
-//prime(x,primes);
