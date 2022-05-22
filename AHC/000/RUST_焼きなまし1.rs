5c80986bfd6629f2b4033c000cec67948d8a1443
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-05-22 18:36:58 +0900

    Update RUST_焼きなまし1.rs

--- AHC/000/RUST_焼きなまし1.rs
+++ AHC/000/RUST_焼きなまし1.rs
@@ -1,20 +1,30 @@
 fn solve(input: &Input) -> Vec<usize> {
+  //焼きなましの温度の始点
   const T0: f64 = 2e3;
+  //焼きなましの温度の終点
   const T1: f64 = 6e2;
+  //焼きなましのタイムリミット
   const TL: f64 = 1.9;
+  //
   let mut rng = rand_pcg::Pcg64Mcg::new(890482);
   let mut state = State::new(input, (0..input.D).map(|_| rng.gen_range(0, 26)).collect());
   let mut cnt = 0;
   let mut T = T0;
   let mut best = state.score;
   let mut best_out = state.out.clone();
+  //焼きなましの処理
   loop {
+    //焼きなまし中の乱択回数
     cnt += 1;
+    //乱択回数が100の倍数になったら時間が大丈夫か判定する
     if cnt % 100 == 0 {
+      //t:タイムリミット1.9sに対する経過時間の割合(get_time()でプログラムの開始からの時間をとる)
       let t = get_time() / TL;
+      //プログラム開始から1.9second経過している場合は焼きなましを終えて出力に移る
       if t >= 1.0 {
         break;
       }
+      //焼きなましの現時点での温度(乱択時の悪化を受け入れるかどうかの確率)を現在の経過時間を用いて変動させる
       T = T0.powf(1.0 - t) * T1.powf(t);
     }
     let old_score = state.score;
