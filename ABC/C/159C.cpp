2feba276717904abd889bf1b2b03f23b024facb7
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-06-06 10:48:47 +0900

    Update 159C.cpp

--- ABC/C/159C.cpp
+++ ABC/C/159C.cpp
@@ -1,12 +1,24 @@
 #include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
 using namespace std;
-#define rep(i,N) for(int i = 0; i < N; i++)
-typedef long long ll;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+using TP = tuple<ll,ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
 int main(){
-    double l;
-    cin >> l;
-    double ans = 0;
-    ans = (l / 3) * (l / 3) * (l / 3);
-    printf("%.10f\n",ans);
-    return 0;
+  double l;
+  cin >> l;
+  double l2 = l / (double)3;
+  printf("%.10f\n",l2*l2*l2);
+  return 0;
 }
