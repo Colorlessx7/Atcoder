62199d00399a9902fd561448232072d4ac0ee65b
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-06-07 14:57:36 +0900

    Update 126C.cpp

--- ABC/C/126C.cpp
+++ ABC/C/126C.cpp
@@ -2,44 +2,38 @@
 #include<atcoder/all>
 using namespace atcoder;
 using namespace std;
-#define rep(i,N) for(int i = 0; i < N; i++)
-#define rep2(i,N) for(int i = 1; i <= N; i++)
-#define rep3(i,N) for(int i = N - 1; i >= 0; i--)
-#define rep4(i,N) for(int i = N; i > 0; i--)
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
 #define all(x) x.begin(),x.end() 
-typedef long long ll;
-using P = pair<int,int>;
-int cnt(int ret, int n,int k){
-    while(n < k){
-        n *= 2;
-        ret++;
-    }
-    return ret;
-}
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+using TP = tuple<ll,ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
 int main(){
-    ll n, k;
-    cin >> n >> k;
-    vector<int> d;
-    double ans = 0;
-    int x = 0;
-    rep2(i,n){
-        int f = cnt(0,i,k);
-        if(f == 0){
-            x++;
-            continue;
-        }
-        f = 1 << f;
-        d.push_back(f); 
+  ll n, k;
+  cin >> n >> k;
+  double num = 0;
+  rep2(i,n){
+    ll x = i;
+    ll cnt = 0;
+    while(x < k){
+      x *= 2;
+      cnt++; 
     }
-    int y = d.size();
-    rep(i,y){
-        ans += (double)1 / d[i];
+    double m = 1;
+    rep(j,cnt){
+      m *= 2;
     }
-    rep(i,x){
-        ans += (double)1;
-    }
-    ans /= (double)n;
-    printf("%.10f\n",ans);
-    return 0;
+    double f = (double)1/m;
+    num += f; 
+  }
+  num /= n;
+  printf("%.12f\n",num);
+  return 0;
 }
-//確率計算問題
