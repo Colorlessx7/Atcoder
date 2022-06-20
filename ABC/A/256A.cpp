d9ef9ceed284f05038cbf7f647a6ef44f6806e7c
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-06-20 14:53:55 +0900

    Create 256A.cpp

---
+++ ABC/A/256A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(int i = 0; i < N; i++)
+#define rep2(i,N) for(int i = 1; i <= N; i++)
+#define rep3(i,N) for(int i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(int i = N; i > 0; i--)
+#define replr(i,l,r) for(int i = l; i < r; i++)
+#define reprl(i,l,r) for(int i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<int,int>;
+using TP = tuple<int,int,int>;
+void chmin(int &x, int y){ x = min(x,y); }
+void chmax(int &x, int y){ x = max(x,y); }
+int main(){
+  int n;
+  cin >> n;
+  ll ans = 1;
+  rep(i,n){
+    ans *= 2;
+  }
+  cout << ans << endl;
+  return 0;
+}
