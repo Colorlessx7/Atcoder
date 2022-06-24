124bda579a574d485fd5ea0ccd2dfbc05cf90fc2
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-06-24 16:08:52 +0900

    Create 253A.cpp

---
+++ ABC/A/253A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(ll i = 0; i < N; i++)
+#define rep2(i,N) for(ll i = 1; i <= N; i++)
+#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(ll i = N; i > 0; i--)
+#define replr(i,l,r) for(ll i = l; i < r; i++)
+#define reprl(i,l,r) for(ll i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<ll,ll>;
+using TP = tuple<ll,ll,ll>;
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  vector<ll> a(3);
+  ll n;
+  rep(i,3){
+    cin >> a[i];
+    if(i == 1){
+      n = a[i];
+    }
+  }
+  sort(all(a));
+  if(a[1] == n){
+    cout << "Yes" << endl;
+  }else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
