8913f891c4e8945e7827f97f9e7e3452de764228
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-07-16 16:18:42 +0900

    Create 259B.cpp

---
+++ ABC/B/259B.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(int i = 0; i < N; i++)
+#define rep2(i,N) for(int i = 1; i <= N; i++)
+#define rep3(i,N) for(int i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(int i = N; i > 0; i--)
+#define replr(i,l,r) for(int i = l; i < r; i++)
+#define reprl(i,l,r) for(int i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+using ll = long long;
+using P = pair<int,int>;
+using TP = tuple<int,int,int>;
+void chmin(int &x, int y){ x = min(x,y); }
+void chmax(int &x, int y){ x = max(x,y); }
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  double a, b, d;
+  cin >> a >> b >> d;
+  double r = a * a + b * b;
+  r = sqrt(r);
+  double PI = acos(-1);
+  double rd = atan2(b,a);
+  double rad =  d * PI / (double)180;
+  rd += rad;
+  double i = r * sin(rd);
+  double j = r * cos(rd);
+  printf("%.20f %.20f\n",j,i);
+  return 0;
+}
