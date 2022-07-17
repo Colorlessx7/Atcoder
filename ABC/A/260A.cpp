755f0a132a5e57623f9a578e61a1299698e30bf9
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-07-17 23:12:41 +0900

    Create 260A.cpp

---
+++ ABC/A/260A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(int i = 0; i < N; i++)
+#define rep2(i,N) for(int i = 1; i <= N; i++)
+#define rep3(i,N) for(int i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(int i = N; i > 0; i--)
+#define replr(i,l,r) for(int i = l; i < r; i++)
+#define reprl(i,l,r) for(int i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+#define sz(v) (int)v.size()
+using ll = long long;
+using P = pair<int,int>;
+using TP = tuple<int,int,int>;
+template<typename Tx, typename Ty>Tx dup(Tx x, Ty y){return (x+y-1)/y;}
+void chmin(int &x, int y){ x = min(x,y); }
+void chmax(int &x, int y){ x = max(x,y); }
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  string s;
+  cin >> s;
+  map<char,int> mp;
+  rep(i,3){
+    mp[s[i]]++;
+  }
+  for(auto p : mp){
+    if(p.second == 1){
+      cout << p.first << endl;
+      return 0;
+    }
+  }
+  cout << -1 << endl;
+  return 0;
+}
