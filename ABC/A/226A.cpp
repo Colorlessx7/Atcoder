b5910be86730baec1ea25a69e672dcb14ace6052
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-07-21 17:51:21 +0900

    Create 226A.cpp

---
+++ ABC/A/226A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(int i = 0; i < N; i++)
+#define rep2(i,N) for(int i = 1; i <= N; i++)
+#define rep3(i,N) for(int i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(int i = N; i > 0; i--)
+#define replr(i,l,r) for(int i = l; i < r; i++)
+#define reprl(i,l,r) for(int i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+#define sz(v) (int)v.size()
+using ll = long long;
+using P = pair<int,int>;
+using TP = tuple<int,int,int>;
+template<typename Tx, typename Ty>Tx dup(Tx x, Ty y){return (x+y-1)/y;}
+void chmin(int &x, int y){ x = min(x,y); }
+void chmax(int &x, int y){ x = max(x,y); }
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  int a, b;
+  //scanfで小数をa.bの形で入力
+  //(scanfの入力は変数に代入する場合&aのように&をつける必要がある)
+  scanf("%d.%d",&a,&b);
+  //実数部を答えとする
+  int ans = a;
+  //小数点1桁目をbnとする
+  int bn = b / 100;
+  //bnが５以上なら四捨五入
+  if(bn >= 5){
+    //実数部を+1
+    ans++;
+  }
+  cout << ans << endl;
+  return 0;
+}
