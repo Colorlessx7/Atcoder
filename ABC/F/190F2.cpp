42fd55a76d5938317b6f2a0cec8350621839d586
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-07-21 18:46:00 +0900

    Create 190F2.cpp

---
+++ ABC/F/190F2.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+using namespace atcoder;
+using namespace std;
+#define rep(i,N) for(int i = 0; i < N; i++)
+#define rep2(i,N) for(int i = 1; i <= N; i++)
+#define rep3(i,N) for(int i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(int i = N; i > 0; i--)
+#define replr(i,l,r) for(int i = l; i < r; i++)
+#define reprl(i,l,r) for(int i = l; i >= r; i--)
+#define all(x) x.begin(),x.end() 
+#define allr(x) x.rbegin(),x.rend() 
+#define sz(v) (int)v.size()
+using ll = long long;
+using P = pair<int,int>;
+using TP = tuple<int,int,int>;
+template<typename Tx, typename Ty>Tx dup(Tx x, Ty y){return (x+y-1)/y;}
+void chmin(int &x, int y){ x = min(x,y); }
+void chmax(int &x, int y){ x = max(x,y); }
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
+int main(){
+  int n;
+  cin >> n;
+  fenwick_tree<ll> fw(n+2);
+  vector<int> a(n);
+  ll ans = 0;
+  rep(i,n){
+    cin >> a[i];
+    fw.add(a[i],1);
+    ans += fw.sum(a[i]+1,n+1);
+  }
+  rep(i,n){
+    cout << ans << endl;
+    ans -= a[i];
+    ans += n-a[i]-1;
+  }
+  return 0;
+}
+//VC_7_20の解
