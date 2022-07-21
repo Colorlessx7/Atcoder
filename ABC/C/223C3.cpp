ae7edd5560107ccae9995fdc6de9ff99f24123ae
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-07-21 20:59:07 +0900

    Update 223C3.cpp

--- ABC/C/223C3.cpp
+++ ABC/C/223C3.cpp
@@ -20,16 +20,25 @@ void chmax(int &x, int y){ x = max(x,y); }
 void chmin(ll &x, ll y){ x = min(x,y); }
 void chmax(ll &x, ll y){ x = max(x,y); }
 int main(){
+  //要素数
   int n;
   cin >> n;
+  //時間に関する累積和
   vector<double> s(n+1);
+  //長さに関する累積和
   vector<double> len(n+1);
+  //入力 a:道のり,b:速さ
   vector<double> a(n),b(n);
+  //累積和の作成
   rep(i,n){
     cin >> a[i] >> b[i];
+    //長さの累積和に導火線の長さを加算
     len[i+1] = len[i] + a[i];
+    //時間の累積和に導火線を焼き切るまでの時間を加算
+    //時間=道のりa/速さb
     s[i+1] += s[i] + (a[i]/b[i]);
   }
+  //二つの火がぶつかる時間を計算(時間の累積和の最後の値/2)
   double time = s[n]/2;
   int it = upper_bound(all(s),time) - s.begin();
   it--;
