f612d818b8b64f04b029d157431053235f35fcb6
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-07-23 16:04:46 +0900

    Update 177D.cpp

--- ABC/D/177D.cpp
+++ ABC/D/177D.cpp
@@ -2,32 +2,43 @@
 #include<atcoder/all>
 using namespace atcoder;
 using namespace std;
-#define rep(i,N) for(ll i = 0; i < N; i++)
-#define rep2(i,N) for(ll i = 1; i <= N; i++)
-#define rep3(i,N) for(ll i = N - 1; i >= 0; i--)
-#define rep4(i,N) for(ll i = N; i > 0; i--)
-#define replr(i,l,r) for(ll i = l; i < r; i++)
-#define reprl(i,l,r) for(ll i = l; i > r; i--)
+#define rep(i,N) for(int i = 0; i < N; i++)
+#define rep2(i,N) for(int i = 1; i <= N; i++)
+#define rep3(i,N) for(int i = N - 1; i >= 0; i--)
+#define rep4(i,N) for(int i = N; i > 0; i--)
+#define replr(i,l,r) for(int i = l; i < r; i++)
+#define reprl(i,l,r) for(int i = l; i >= r; i--)
 #define all(x) x.begin(),x.end() 
 #define allr(x) x.rbegin(),x.rend() 
-typedef long long ll;
-using P = pair<ll,ll>;
+#define sz(v) (int)v.size()
+using ll = long long;
+using P = pair<int,int>;
+using TP = tuple<int,int,int>;
+template<typename Tx, typename Ty>Tx dup(Tx x, Ty y){return (x+y-1)/y;}
+void chmin(int &x, int y){ x = min(x,y); }
+void chmax(int &x, int y){ x = max(x,y); }
+void chmin(ll &x, ll y){ x = min(x,y); }
+void chmax(ll &x, ll y){ x = max(x,y); }
 int main(){
-    int n, m;
-    cin >> n >> m;
-    dsu d(n);
-    rep(i,m){
-        int a, b;
-        cin >> a >> b;
-        a--; b--;
-        d.merge(a,b);    
-    }
-    int ans = 0;
-    rep(i,n){
-        int x = d.size(i);
-        ans = max(ans,x);
-    }
-    cout << ans << endl;
-    return 0;
+  int n, m;
+  cin >> n >> m;
+  dsu d(n);
+  //辺の入力
+  rep(i,m){
+    int a, b;
+    cin >> a >> b;
+    //0indexed調整
+    a--; b--;
+    //辺の接続
+    d.merge(a,b);
+  }
+  int ans = 0;
+  //全頂点について回す
+  rep(i,n){
+    //一番大きい結合サイズをansに格納
+    chmax(ans,d.size(i));
+  }
+  cout << ans << endl;
+  return 0;
 }
 //dsu
