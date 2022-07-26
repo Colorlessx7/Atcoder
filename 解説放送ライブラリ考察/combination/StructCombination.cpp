7e456e962279d8791f4ce0a326ec7817009ed427
Colorlessx7 <98252800+Colorlessx7@users.noreply.github.com>
2022-07-26 17:46:57 +0900

    Update StructCombination.cpp

--- 解説放送ライブラリ考察/combination/StructCombination.cpp
+++ 解説放送ライブラリ考察/combination/StructCombination.cpp
@@ -1,18 +1,1 @@
-//using mint = modint1000000007;
-//const int mod = 1000000007;
-//using mint = modint998244353;
-//const int mod = 998244353;
-struct combination {
-  vector<mint> fact, ifact;
-  combination(int n):fact(n+1),ifact(n+1) {
-    assert(n < mod);
-    fact[0] = 1;
-    for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
-    ifact[n] = fact[n].inv();
-    for (int i = n; i >= 1; --i) ifact[i-1] = ifact[i]*i;
-  }
-  mint operator()(int n, int k) {
-    if (k < 0 || k > n) return 0;
-    return fact[n]*ifact[k]*ifact[n-k];
-  }
-};
+https://github.com/atcoder/live_library/blob/master/comb.cpp
